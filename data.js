7964acf85b8bcae8e1f62ad89beb76d23a222981
sscMock <xtraspaceforuse@gmail.com>
2026-01-19 15:41:03 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+const products = [
+  { id: 1, name: "Brazil 2002 Retro", price: 299, img: "https://via.placeholder.com/300x400" },
+  { id: 2, name: "Argentina 1986 Retro", price: 349, img: "https://via.placeholder.com/300x400" },
+  { id: 3, name: "Man United 1999", price: 399, img: "https://via.placeholder.com/300x400" },
+  { id: 4, name: "Barcelona 2010", price: 379, img: "https://via.placeholder.com/300x400" }
+];
