2df4f17befed5d714ba8e48a11b5da79bbb5bbef
sscMock <xtraspaceforuse@gmail.com>
2026-01-19 15:41:41 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+const grid = document.getElementById("product-grid");
+
+if (grid) {
+  products.forEach(p => {
+    grid.innerHTML += `
+      <div class="product">
+        <img src="${p.img}">
+        <h3>${p.name}</h3>
+        <p>₹${p.price}</p>
+        <button onclick="addToCart(${p.id})">Add to Cart</button>
+      </div>
+    `;
+  });
+}
