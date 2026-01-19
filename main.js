3a193e342694009477bf18733d90fde3b10568ca
sscMock <xtraspaceforuse@gmail.com>
2026-01-19 15:46:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ if (grid) {
         <img src="${p.img}">
         <h3>${p.name}</h3>
         <p>₹${p.price}</p>
-        <button onclick="addToCart(${p.id})">Add to Cart</button>
+        <button onclick="location.href='product.html?id=${p.id}'">View Product</button>
       </div>
     `;
   });
